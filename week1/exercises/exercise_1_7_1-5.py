adc5624b9742422f5ce74310d80c52e15f69ab69
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-09-22 15:50:02 +0200

    exercises week 1

---
+++ week1/exercises/exercise_1_7_1-5.py
@@ -0,0 +1,37 @@
+# I don't expect you to put in all the extra print statement
+# they are there to help follow my train of though
+print(9 * "*", " + ", 9 * "*")
+print(3 + 5)
+print("Hello " + "world!")
+
+print(9 * "*", " - ", 9 * "*")
+print(5 - 3)
+
+print(9 * "*", " * ", 9 * "*")
+print(3 * 5)
+print(3 * "Hallo")
+
+print(9 * "*", " / ", 9 * "*")
+print(5 / 3)
+
+print(9 * "*", " // ", 8 * "*")
+print(5 // 3)
+print(6 // 3)
+print(7 // 3)
+# this give the integer division
+
+print(9 * "*", " % ", 9 * "*")
+print(5 % 3)
+print(6 % 3)
+print(7 % 3)
+# this gives the remainder after integer division
+
+print(9 * "*", " ** ", 8 * "*")
+print(2 ** 2)
+print(2 ** 3)
+print(2 ** 4)
+print(3 ** 2)
+# this is "to the power"
+
+# exercise 2
+print("This is a string" + 5)
